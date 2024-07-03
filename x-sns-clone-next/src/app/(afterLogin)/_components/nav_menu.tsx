126233cce0af369545acef3112e6b0f17aceb1c4
joanna930224 <annavu0224@gmail.com>
2024-07-03 11:39:25 +0900

    chore : Nav menu 선택시 bold 처리

--- x-sns-clone-next/src/app/(afterLogin)/_components/nav_menu.tsx
+++ x-sns-clone-next/src/app/(afterLogin)/_components/nav_menu.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import style from "@/app/(afterLogin)/_components/nav_menu.module.css";
 import Link from "next/link";
@@ -5,8 +7,11 @@ import { GoHomeFill } from "react-icons/go";
 import { FiSearch } from "react-icons/fi";
 import { FiMail } from "react-icons/fi";
 import { GoPersonFill } from "react-icons/go";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 const NavMenu = () => {
+  const segment = useSelectedLayoutSegment();
+
   // 더미데이터
   const me = {
     id: "kun_0426",
@@ -17,26 +22,62 @@ const NavMenu = () => {
       <li>
         <Link href="/home">
           <div className={style.navItem}>
-            <GoHomeFill size={24} />
-            <div>Home</div>
+            {segment === "home" ? (
+              <>
+                <GoHomeFill size={24} />
+                <div style={{ fontWeight: "bold" }}>Home</div>
+              </>
+            ) : (
+              <>
+                <GoHomeFill size={24} />
+                <div>Home</div>
+              </>
+            )}
           </div>
         </Link>
         <Link href="/explore">
           <div className={style.navItem}>
-            <FiSearch size={24} />
-            <div>Explore</div>
+            {segment === "explore" ? (
+              <>
+                <FiSearch size={24} />
+                <div style={{ fontWeight: "bold" }}>Explore</div>
+              </>
+            ) : (
+              <>
+                <FiSearch size={24} />
+                <div>Explore</div>
+              </>
+            )}
           </div>
         </Link>
         <Link href="/messages">
           <div className={style.navItem}>
-            <FiMail size={24} />
-            <div>Messages</div>
+            {segment === "messages" ? (
+              <>
+                <FiMail size={24} />
+                <div style={{ fontWeight: "bold" }}>Messages</div>
+              </>
+            ) : (
+              <>
+                <FiMail size={24} />
+                <div>Messages</div>
+              </>
+            )}
           </div>
         </Link>
         <Link href={`/${me?.id}`}>
           <div className={style.navItem}>
-            <GoPersonFill size={24} />
-            <div>Profile</div>
+            {segment === me.id ? (
+              <>
+                <GoPersonFill size={24} />
+                <div style={{ fontWeight: "bold" }}>Profile</div>
+              </>
+            ) : (
+              <>
+                <GoPersonFill size={24} />
+                <div>Profile</div>
+              </>
+            )}
           </div>
         </Link>
       </li>
