c4e0c9936c46b97775ae702514f70b12b867bff5
joanna930224 <annavu0224@gmail.com>
2024-07-03 11:23:35 +0900

    feat : Search Page UI

--- x-sns-clone-next/src/app/(afterLogin)/search/page.tsx
+++ x-sns-clone-next/src/app/(afterLogin)/search/page.tsx
@@ -1,7 +1,40 @@
-export default function Page() {
+import BackButton from "../_components/back_button";
+import Post from "../_components/post";
+import SearchForm from "../_components/search_form";
+import Tab from "../home/_components/tab";
+import style from "./search.module.css";
+
+type Props = {
+  searchParams: { q: string; f?: string; pf?: string };
+};
+
+export default function Page({ searchParams }: Props) {
   return (
-    <main>
-      검색
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </main>
-  )
+  );
 }
